a10adc602fb8290b1301194f976080109dd32898
XuanNguyen-UET <22022670@vnu.edu.vn>
2024-01-24 17:12:29 +0700

    change test

--- tree/test_tree.js
+++ tree/test_tree.js
@@ -6,4 +6,14 @@ const arr = [3, 4, 1, 0, 6, 5, 2]
 
 const btree = new BinaryTree(arr)
 console.log(btree);
+/**
+ * The output should return:
+ *          3 <- root
+ *         /\
+ *        1 4 
+ *       /\  \
+ *      0 2  6
+ *          /
+ *         5 <- leaf  
+ */
 console.log(btree.height());
